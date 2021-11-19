ff47004001bdee0c9e7f36366d49a46d34290f79
Anemaygi <gianemayumisan@gmail.com>
2021-11-19 01:46:29 -0300

    bagunça em props

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import './App.css';
 import { Conteudo } from './components/Conteudo';
 import { Titulo } from './components/Titulo';
 
+
+const Diverti= (props) => {
+  // modifying the skills array
+  const ListarDiver = props.divertidamentes.map((divertidamente) => <li>{divertidamente}</li>)
+  return <ul>{ListarDiver}</ul>
+}
+
 function App() {
   
   const divertidamentes = ([
@@ -10,12 +17,14 @@ function App() {
     { nome: "algo2", descricao: 'Sim2' },
   ]);
 
+
+
   return (
     <div className="App">
       <header className="App-header">
         <Titulo />
         <Conteudo />
-        
+        <Diverti divertidamentes={["aaa","bbb","ccc"]} />
         {divertidamentes.map((item)=>(
           <>
           <label>{item.nome}</label>
